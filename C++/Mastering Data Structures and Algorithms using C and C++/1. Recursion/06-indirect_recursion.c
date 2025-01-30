fccb29732c56685365dd9847ff3b6807de0e72d9
Dream2503 <swapnarajmohanty2503.com>
2025-01-31 01:21:07 +0530

    commit

--- C++/Mastering Data Structures and Algorithms using C and C++/1. Recursion/06-indirect_recursion.c
+++ C++/Mastering Data Structures and Algorithms using C and C++/1. Recursion/06-indirect_recursion.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-void func2(int n);
-void func1(int n) {
+void funB(int n);
+
+void funA(int n) {
     if (n > 0) {
         printf("%d ", n);
-        func2(n-1);
-    }
-}
-void func2(int n) {
-    if (n > 1) {
-        printf("%d ", n);
-        func1(n/2);
+        funB(n - 1);
     }
 }
 
-int main() {
-    func1(20);
+void funB(int n) {
+    if (n > 1) {
+        printf("%d ", n);
+        funA(n / 2);
+    }
+}
+
+int main(){
+    funA(20);
     return 0;
 }
