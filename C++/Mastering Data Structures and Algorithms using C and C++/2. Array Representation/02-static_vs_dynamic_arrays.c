3789ba296f186c82ce5cd5ecd6b5f2cb48231aeb
Dream2503 <swapnarajmohanty2503.com>
2025-02-08 22:32:18 +0530

    commit

--- C++/Mastering Data Structures and Algorithms using C and C++/2. Array Representation/02-static_vs_dynamic_arrays.c
+++ C++/Mastering Data Structures and Algorithms using C and C++/2. Array Representation/02-static_vs_dynamic_arrays.c
@@ -3,22 +3,22 @@
 
 int main() {
     int A[5] = {2, 4, 6, 8, 10};
-    int *p;
+    int *p, i;
 
-    p = (int*)malloc(5*sizeof(int));
+    p = (int*)malloc(5 * sizeof(int));
     p[0] = 3;
     p[1] = 5;
     p[2] = 7;
     p[3] = 9;
     p[4] = 11;
 
-    for (int i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++) {
         printf("%d ", A[i]);
-    
+    }
     printf("\n");
 
-    for (int i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++) {
         printf("%d ", p[i]);
-
+    }
     return 0;
 }
--- C++/Mastering Data Structures and Algorithms using C and C++/2. Array Representation/03-array_size.c
+++ C++/Mastering Data Structures and Algorithms using C and C++/2. Array Representation/03-array_size.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 int main() {
-    int *p, *q;
-    p = (int*)malloc(5*sizeof(int));
+    int *p, *q, i;
+    p = (int*)malloc(5 * sizeof(int));
     p[0] = 3; p[1] = 5; p[2] = 7; p[3] = 9; p[4] = 11;
 
-    q = (int*)malloc(10*sizeof(int));
+    q = (int*)malloc(10 * sizeof(int));
 
-    for (int i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++) {
         q[i] = p[i];
-
+    }
     free(p);
     p = q;
     q = NULL;
 
-    for (int i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++) {
         printf("%d ", p[i]);
-    
+    }
     return 0;
 }
