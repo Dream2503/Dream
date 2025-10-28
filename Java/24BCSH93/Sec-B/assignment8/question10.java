ae84cbd369950285c39f947cd3720c0794ad32ad
Dream <swapnarajmohanty2503@gmail.com>
2025-10-28 18:31:01 +0530

    commit

--- Java/24BCSH93/Sec-B/assignment8/question10.java
+++ Java/24BCSH93/Sec-B/assignment8/question10.java
@@ -3,3 +3,55 @@
          variable should contain the result. The program should use a Synchronized block to make sure that only one thread is updating the global
          variable at a given time.
 */
+
+import java.util.Scanner;
+
+class SumOfNaturalNumber implements Runnable {
+    private int n, i = 1, sum = 0;
+
+    public SumOfNaturalNumber(int n) throws IllegalArgumentException {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Enter valid Natural Number. n: " + n);
+        }
+        this.n = n;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void run() {
+        while (i <= n) {
+            synchronized (this) {
+                sum += i++;
+            }
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to calculate the sum: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the number of threads: ");
+        int m = sc.nextInt();
+        SumOfNaturalNumber sumOfNaturalNumber = new SumOfNaturalNumber(n);
+        Thread[] threads = new Thread[m];
+
+        for (int i = 0; i < m; i++) {
+            threads[i] = new Thread(sumOfNaturalNumber);
+        }
+        for (int i = 0; i < m; i++) {
+            threads[i].start();
+        }
+        for (int i = 0; i < m; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                System.out.println("Thread was interrupted");
+            }
+        }
+        System.out.println("Sum of the first " + n + " Natural Numbers are: " + sumOfNaturalNumber.getSum());
+    }
+}
