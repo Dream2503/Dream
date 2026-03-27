9e6829f94f2a24b804843766196caab5bac12183
Dream2503 <swapnarajmohanty2503@gmail.com>
2026-03-27 19:52:59 +0530

    commit

--- PostgreSQL/assignment7.sql
+++ PostgreSQL/assignment7.sql
@@ -1,20 +1,101 @@
-drop table employee CASCADE;
-drop table department CASCADE;
-drop table dept_locations CASCADE;
-drop table project CASCADE;
-drop table works_on CASCADE;
-
--- employee
+/*
+Q1. Create employee table with the following constraints.
+    a) Ename should not be null values .
+    b) Esrno should accept 6 digits only.
+    c) Bdate should store the years from 1955 to 1979.
+    d) Sex should be by default ‘M’.
+    e) Salary should be between 20000 to 40000.
+    f) Mgrsrno should be foreign key for employee table esrno.
+*/
 CREATE TABLE employee (
-    ename VARCHAR(16) NOT NULL,
-    esrno INT PRIMARY KEY CHECK (esrno BETWEEN 100000 AND 999999),
-    bdate DATE CHECK (EXTRACT(YEAR FROM bdate) BETWEEN 1955 AND 1979),
+    ename   VARCHAR(16) NOT NULL,
+    esrno   INT PRIMARY KEY CHECK (esrno BETWEEN 100000 AND 999999),
+    bdate   DATE CHECK (EXTRACT(YEAR FROM bdate) BETWEEN 1955 AND 1979),
     address VARCHAR(16),
-    sex CHAR(1) DEFAULT 'M',
-    salary INT CHECK (salary BETWEEN 20000 AND 40000),
+    sex     CHAR(1) DEFAULT 'M',
+    salary  INT CHECK (salary BETWEEN 20000 AND 40000),
     mgrsrno INT,
-    dno INT
+    dno     INT
 );
+
+/*
+Q2. Create department table with the following constraints.
+    a) Dname should be (RESEARCH, ADMIN, PROJECT, ACADEMIC) (case Sensitive)
+    b) Dnumber should be positive and <= 4 only.
+*/
+CREATE TABLE department (
+    dname     VARCHAR(16) CHECK (dname IN ('RESEARCH', 'ADMIN', 'PROJECT', 'ACADEMIC')),
+    dnumber   INT PRIMARY KEY CHECK (dnumber BETWEEN 1 AND 4),
+    mgrsrno   INT,
+    mgrstartd DATE
+);
+
+/*
+Q3. Create Department locations table with the following constraints.
+    a) Dnumber should be positive and <= 4 only.
+    b) Dlocations are (SINGAPORE, INDIA, QUUENSLAND, LONDON).
+*/
+CREATE TABLE dept_locations (
+    dnumber   INT CHECK (dnumber BETWEEN 1 AND 4),
+    dlocation VARCHAR(16) CHECK (dlocation IN ('SINGAPORE', 'INDIA', 'QUUENSLAND', 'LONDON'))
+);
+
+/*
+Q4. Create project table with the following constraints.
+    a) Pname should be (NETWORKING, BIO INFORMATICS, LINUX).
+    b) Plocation should be (KOREA, SOUTH AFRICA, INDIA).
+*/
+CREATE TABLE project (
+    pname     VARCHAR(16) CHECK (pname IN ('NETWORKING', 'BIO INFORMATICS', 'LINUX')),
+    pnumber   INT PRIMARY KEY,
+    plocation VARCHAR(16) CHECK (plocation IN ('KOREA', 'SOUTH AFRICA', 'INDIA')),
+    dnum      INT
+);
+
+/*
+Q5. Create table works_on with the following constraints.
+    a) all the coloumns should be > 0.
+*/
+CREATE TABLE works_on (
+    esrno INT CHECK (esrno BETWEEN 100000 AND 999999),
+    pno   INT CHECK (pno > 0),
+    hours NUMERIC(3, 2) CHECK (hours > 0)
+);
+
+ALTER TABLE employee
+    ADD CONSTRAINT fk_mgrsrno_esrno
+        FOREIGN KEY (mgrsrno)
+            REFERENCES employee (esrno);
+
+ALTER TABLE employee
+    ADD CONSTRAINT fk_dno_dnumber
+        FOREIGN KEY (dno)
+            REFERENCES department (dnumber);
+
+ALTER TABLE department
+    ADD CONSTRAINT fk_mgrsrno_esrno
+        FOREIGN KEY (mgrsrno)
+            REFERENCES employee (esrno);
+
+ALTER TABLE dept_locations
+    ADD CONSTRAINT fk_dnumber_dnumber
+        FOREIGN KEY (dnumber)
+            REFERENCES department (dnumber);
+
+ALTER TABLE project
+    ADD CONSTRAINT fk_dnum_dnumber
+        FOREIGN KEY (dnum)
+            REFERENCES department (dnumber);
+
+ALTER TABLE works_on
+    ADD CONSTRAINT fk_esrno_esrno
+        FOREIGN KEY (esrno)
+            REFERENCES employee (esrno);
+
+ALTER TABLE works_on
+    ADD CONSTRAINT fk_pno_pnumber
+        FOREIGN KEY (pno)
+            REFERENCES project (pnumber);
 
 INSERT INTO employee
 VALUES ('AJIT NAYAK', 133100, '1955-04-25', '73 BOSTON', 'M', 35000, NULL, 1),
@@ -27,25 +108,11 @@ VALUES ('AJIT NAYAK', 133100, '1955-04-25', '73 BOSTON', 'M', 35000, NULL, 1),
        ('NIHAR NAYAK', 334524, '1966-12-17', '73 DALLAS', 'M', 29105, 315152, 3),
        ('DEBASMITA', 295485, '1970-04-16', '1 QUEENS LAND', 'F', 20500, 216852, 2);
 
--- department
-CREATE TABLE department (
-    dname VARCHAR(16) CHECK (dname IN ('RESEARCH', 'ADMIN', 'PROJECT', 'ACADEMIC')),
-    dnumber INT PRIMARY KEY CHECK (dnumber BETWEEN 1 AND 4),
-    mgrsrno INT,
-    mgrstartd DATE
-);
-
-INSERT INTO department 
+INSERT INTO department
 VALUES ('RESEARCH', 4, 495823, '1999-04-20'),
        ('ADMIN', 1, 133100, '1988-01-06'),
        ('PROJECT', 2, 216852, '1999-07-17'),
        ('ACADEMIC', 3, 315152, '1999-12-20');
-
--- dept_locations
-CREATE TABLE dept_locations (
-    dnumber INT CHECK (dnumber BETWEEN 1 AND 4),
-    dlocation VARCHAR(16) CHECK (dlocation IN ('SINGAPORE', 'INDIA', 'QUUENSLAND', 'LONDON'))
-);
 
 INSERT INTO dept_locations
 VALUES (2, 'SINGAPORE'),
@@ -53,25 +120,10 @@ VALUES (2, 'SINGAPORE'),
        (4, 'QUUENSLAND'),
        (3, 'LONDON');
 
--- project
-CREATE TABLE project (
-    pname VARCHAR(16) CHECK (pname IN ('NETWORKING', 'BIO INFORMATICS', 'LINUX')),
-    pnumber INT PRIMARY KEY,
-    plocation VARCHAR(16) CHECK (plocation IN ('KOREA', 'SOUTH AFRICA', 'INDIA')),
-    dnum INT
-);
-
 INSERT INTO project
 VALUES ('NETWORKING', 11, 'KOREA', 4),
-       ('BIO INFORMATICS',19, 'SOUTH AFRICA', 3),
+       ('BIO INFORMATICS', 19, 'SOUTH AFRICA', 3),
        ('LINUX', 17, 'INDIA', 2);
-
--- works_on
-CREATE TABLE works_on (
-    esrno INT CHECK (esrno BETWEEN 100000 AND 999999),
-    pno INT CHECK (pno > 0),
-    hours NUMERIC(3, 2) CHECK (hours > 0)
-);
 
 INSERT INTO works_on
 VALUES (315152, 19, 1.25),
@@ -88,38 +140,3 @@ VALUES (315152, 19, 1.25),
        (495823, 11, 3.14),
        (315152, 19, 2.05),
        (334548, 19, 2.5);
-
-ALTER TABLE employee
-ADD CONSTRAINT fk_mgrsrno_esrno
-FOREIGN KEY (mgrsrno)
-REFERENCES employee (esrno);
-
-ALTER TABLE employee
-ADD CONSTRAINT fk_dno_dnumber
-FOREIGN KEY (dno)
-REFERENCES department (dnumber);
-
-ALTER TABLE department
-ADD CONSTRAINT fk_mgrsrno_esrno
-FOREIGN KEY (mgrsrno)
-REFERENCES employee (esrno);
-
-ALTER TABLE dept_locations
-ADD CONSTRAINT fk_dnumber_dnumber
-FOREIGN KEY (dnumber)
-REFERENCES department (dnumber);
-
-ALTER TABLE project
-ADD CONSTRAINT fk_dnum_dnumber
-FOREIGN KEY (dnum)
-REFERENCES department (dnumber);
-
-ALTER TABLE works_on
-ADD CONSTRAINT fk_esrno_esrno
-FOREIGN KEY (esrno)
-REFERENCES employee (esrno);
-
-ALTER TABLE works_on
-ADD CONSTRAINT fk_pno_pnumber
-FOREIGN KEY (pno)
-REFERENCES project (pnumber);
